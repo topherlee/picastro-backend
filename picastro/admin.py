7cab133f636341f54da069b08b5f4f6bd5c8fe7e
iamEma <t01me22.abdn.ac.uk>
2023-03-04 22:21:27 +0000

    create_database_model

--- picastro/admin.py
+++ picastro/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, StarCamp, UserProfile, Equipment, Imageissaved, Subscription
 
 admin.site.register(Post)
+admin.site.register(StarCamp)
+admin.site.register(UserProfile)
+admin.site.register(Equipment)
+admin.site.register(Imageissaved)
+admin.site.register(Subscription)
+
+
--- picastro/models.py
+++ picastro/models.py
@@ -21,3 +21,52 @@ class Post(models.Model):
 
     def __str__(self):
         return self.poster.username
+
+class StarCamp(models.Model):
+    starCampName = models.TextField()
+    starCamplocation = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.starCampName
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100, blank=True)
+    starCampId = models.ForeignKey(StarCamp, on_delete=models.CASCADE)
+    subcriptionsExpiry = models.DateTimeField(auto_now_add=True)
+    isEmailVerified = models.BooleanField()
+    userDescription = models.TextField()
+   
+    def __str__(self):
+        return self.user.username
+
+class Equipment(models.Model):
+    setName = models.TextField()
+    telescopeName = models.TextField()
+    cameraName = models.TextField()
+    guideCameraName = models.TextField()
+    offAxisguidecamera = models.TextField()
+    filterWheelName = models.TextField()
+    filters = models.TextField()
+    barlowLense = models.TextField()
+    otherEquipment = models.TextField()
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.setName
+    
+
+class Imageissaved(models.Model):
+    userId= models.ForeignKey(User, on_delete=models.CASCADE)
+    imageIsSaved = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.imageIsSaved
+
+class Subscription(models.Model):
+    subcriptionsPlan = models.TextField()
+    subcriptionsDuration = models.DurationField()
+    subcriptionsPrice = models.DecimalField(max_digits = 5, decimal_places = 2)
+
+    def __str__(self):
+        return self.subcriptionsPlan
