ccf6043fc4accc79b03b78bef1323d798ca8cae7
Christopher Lee <99973434+topherlee@users.noreply.github.com>
2023-03-20 21:26:14 +0000

    added aws allowed host

--- picastro_backend/settings.py
+++ picastro_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-y6qmxm&3awp)*unz^*&!+97x*z0sa$3-s9t0^@fvkc(@xe$^#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2','13.42.37.75']
 
 
 # Application definition
