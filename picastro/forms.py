d6ad4400f8deae183065c8339f67284533b21a32
t01me22 <100229876+t01me22@users.noreply.github.com>
2023-03-27 13:52:33 +0000

    post_image

---
+++ picastro/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ["image", "imageDescription", "astroNameShort","astroName", "imageIsSaved",
+        "award", "exposureTime","moonPhase", "cloudCoverage","bortle", "starCamp","leadingLight"]
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
